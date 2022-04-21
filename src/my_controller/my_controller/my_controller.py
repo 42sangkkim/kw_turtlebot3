5f543502fff3b9da2336a37058d4f9f747bcf967
sangkkim <madlogic365@gmail.com>
2022-04-21 16:36:53 +0900

    show inage

--- src/my_controller/my_controller/my_controller.py
+++ src/my_controller/my_controller/my_controller.py
@@ -1,5 +1,6 @@
 import rclpy
 import my_controller.comm as comm
+import cv2
 
 def main(args=None):
 	rclpy.init(args=args)
@@ -9,7 +10,10 @@ def main(args=None):
 	# rclpy.spin(my_controller_publisher)
 	rclpy.spin_once(my_controller_subscriber)
 
-	myImage = my_controller_subscriber.GetImage()
+	myImage = my_controller_subscriber.GetImage() # Cv2Image
+	cv2.imgshow("My Image", myImage)
+	cv2.waitKey(0)
+
 	# my_controller_publisher.destry_node()
 	my_controller_subscriber.destroy_node()
 	rclpy.shutdown
