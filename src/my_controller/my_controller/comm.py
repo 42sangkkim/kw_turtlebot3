d543b0a7a7554318aa42836b8ea88779b53bc1af
sangkkim <madlogic365@gmail.com>
2022-04-21 14:17:46 +0900

    edit comm.py String -> Twist

--- src/my_controller/my_controller/comm.py
+++ src/my_controller/my_controller/comm.py
@@ -1,21 +1,21 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 class ClientPublisher(Node):
 	def __init__(self):
-		super().__init__('client_publisher')
-		self.publisher = self.create_publisher(String, 'client2server', 10)
+		super().__init__('my_controller_pub')
+		self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 		timer_period = 0.5
 		self.timer = self.create_timer(timer_period, self.timer_callback)
-		self.i = 0
 
 	def timer_callback(self):
-		msg = String()
-		msg.data = 'Hello Server! %d' % self.i
+		msg = Twist()
+		msg.linear.x = 1.0
+		msg.angular.z = 1.0
 		self.publisher.publish(msg)
-		self.get_logger().info("Publishing: %s" % msg.data)
-		self.i += 1
+		self.get_logger().info("Publishing: %s" % msg)
 
 class ClientSubscriber(Node):
 	def __init__(self):
