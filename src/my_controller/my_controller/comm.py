ed6646a8a657f2a93dc1e0b82227fb7aba3b33f6
sangkkim <madlogic365@gmail.com>
2022-05-03 18:41:16 +0900

    add linear/angular velocity and make set_velocoty func

--- src/my_controller/my_controller/comm.py
+++ src/my_controller/my_controller/comm.py
@@ -11,12 +11,18 @@ class ClientPublisher(Node):
 		super().__init__('my_controller_pub')
 		self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 		timer_period = 0.5
+		self.__linear_velocity = 0.0
+		self.__angular_velocoty = 0.0
 		self.timer = self.create_timer(timer_period, self.timer_callback)
+	
+	def set_velocoty(self, linear_velocoty, angular_velocoty):
+		self.__linear_velocity = linear_velocoty
+		self.__angular_velocoty = angular_velocity
 
 	def timer_callback(self):
 		msg = Twist()
-		msg.linear.x = 1.0
-		msg.angular.z = 1.0
+		msg.linear.x = self.__linear_velocity
+		msg.angular.z = self.__angular_velocoty
 		self.publisher.publish(msg)
 		self.get_logger().info("Publishing: %s" % msg)
 
