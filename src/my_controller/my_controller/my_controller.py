46baa02c76114450f77a5d3a029fff514f70dbcf
sangkkim <madlogic365@gmail.com>
2022-05-03 19:46:56 +0900

    make control algorithm move with w, a, s, d, sp and quit ros with q

--- src/my_controller/my_controller/my_controller.py
+++ src/my_controller/my_controller/my_controller.py
@@ -4,19 +4,31 @@ import cv2
 
 def main(args=None):
 	rclpy.init(args=args)
-	# my_controller_publisher = comm.ClientPublisher()
 	my_controller_subscriber = comm.ClientSubscriber()
+	my_controller_publisher = comm.ClientPublisher()
 
-	# rclpy.spin(my_controller_publisher)
-	rclpy.spin_once(my_controller_subscriber)
+	while(1):
+		rclpy.spin_once(my_controller_publisher)
 
-	myImage = my_controller_subscriber.GetImage() # Cv2Image
-	cv2.namedWindow("My Image", flags=cv2.WINDOW_NORMAL)
-	cv2.resizeWindow(winname="My Image", width=320, height=180)
-	cv2.imshow("My Image", myImage)
-	cv2.waitKey(0)
+		rclpy.spin_once(my_controller_subscriber)
+		myImage = my_controller_subscriber.GetImage() # Cv2Image
+		cv2.imshow("My Image", myImage)
+		cv2.waitKey(0)
+		if (keyin == ord('w')):
+			my_controller_publisher.set_velocity(0.1, 0.0) # move forward
+		elif (keyin == ord('a')):
+			my_controller_publisher.set_velocity(0.0, 0.1) # turn left (counter clock wise)
+		elif (keyin == ord('s')):
+			my_controller_publisher.set_velocity(-0.1, 0.0) # move backward
+		elif (keyin == ord('d')):
+			my_controller_publisher.set_velocity(0.0, -0.1) # turn right (clock wise)
+		elif (keyin == 32): # space bar
+			my_controller_publisher.set_velocity(0.0, 0.0) # stop
 
-	# my_controller_publisher.destry_node()
+		elif (keyin = ord('q')): # quit
+			break
+
+	my_controller_publisher.destry_node()
 	my_controller_subscriber.destroy_node()
 	rclpy.shutdown
 
